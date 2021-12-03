d7f8f8bedfef0c4deefc4c3350bbf05b4a436089
rahulrm33 <rahavarahul23@gmail.com>
2021-12-03 22:11:30 +0530

    config change

--- rest_api-2-main/email/account.js
+++ rest_api-2-main/email/account.js
@@ -1,13 +1,13 @@
 const sgMail=require('@sendgrid/mail')
 
-const sendgridAPIKey ='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'
+const sendgridAPIKey ='' // your customzed key
 
 sgMail.setApiKey(sendgridAPIKey)
 
 const sendWelcomeEmail=(email,name)=>{
     sgMail.send({
         to: email, // Change to your recipient
-        from: 'raghavrahulrm23@gmail.com', // Change to your verified sender
+        from: '', // Email from which id it should send!
         subject: 'Welcome to Issue Tracker appliation! Let’s get started.',
         text: `Hii ${name} ,Happy to have you here . `,
         html: `<strong>Hii ${name} ,Happy to have you here .</strong`,
