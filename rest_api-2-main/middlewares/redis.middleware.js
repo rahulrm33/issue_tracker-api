44e47144f549e08d3e3811b8605aff259a98a62d
rahulrm33 <71714828+rahulrm33@users.noreply.github.com>
2021-12-07 12:33:44 +0530

    Update redis.middleware.js

--- rest_api-2-main/middlewares/redis.middleware.js
+++ rest_api-2-main/middlewares/redis.middleware.js
@@ -7,16 +7,12 @@ function cacheCheck(req, res, next) {
       if (err) {
         throw err;
       } else {
-        // Check cached data is exist.
         if (data != null) {
           // Send cached data.
           console.log('Response is sent from cache with key')
           res.status(200).send(JSON.parse(data));
         } else {
-          // If there is no cached data then let leave the caching to the
-          // final controller function.
           console.log('Response is sent from cache with key to mongo')
-          console.log("start1");
           next();
         }
       }
